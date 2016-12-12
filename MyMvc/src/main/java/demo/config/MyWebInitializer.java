1a49222b0f5874edcc8c6195d443f44cc0c0c7e5
joepis <775679542@qq.com>
2016-12-12 15:25:51 +0800

    使用基于注解+java config配置SpringMvc，代替web.xml配置方式

---
+++ MyMvc/src/main/java/demo/config/MyWebInitializer.java
@@ -0,0 +1,26 @@
+package demo.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+import demo.web.WebConfig;
+
+/**
+ * 配置DispatcherServlet,代替web.xml方式
+ * servlet3.0以下容器（tomcat7以下）只能用web.xml配置
+ * Author LPJ
+ * Date 2016/12/12
+ */
+public class MyWebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class<?>[]{RootConfig.class};
+    }
+
+    // 指定配置类
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class<?>[]{WebConfig.class};
+    }
+
+    // 将DispatcherServlet映射到"/"
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
