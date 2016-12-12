3d3ffba5d1bd74009b6658ffed0d7dd664cabdd0
joepis <775679542@qq.com>
2016-12-12 16:47:57 +0800

    最基本的spring mvc框架搭建，基于注解+java config搭建，代替web.xml配置方式（须servlet3.0及以上）

---
+++ Spittr-basic/src/main/java/spittr/web/WebConfig.java
@@ -0,0 +1,32 @@
+package spittr.web;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc //启用SpringMvc
+@ComponentScan("spittr.web") //启用组件扫描
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    // 配置jsp试图解析器
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/views/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+
+    // 配置静态资源的处理
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
+}
---
+++ Spittr-basic/src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,14 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ page session="false" %>
+<html>
+<head>
+    <title>MyMvc</title>
+    <link rel="stylesheet"
+          type="text/css"
+          href="<c:url value="/resources/style.css" />">
+</head>
+<body>
+<h1>Welcome to Spittr</h1>
+
+</body>
+</html>
